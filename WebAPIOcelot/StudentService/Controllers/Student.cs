7c9d3950312904ea8882c1cfc7f9c056141451f9
sanjay.kumar <sanjay.kumar@taazaa.com>
2023-08-06 22:13:52 +0530

    ocelot api gateway

---
+++ WebAPIOcelot/StudentService/Controllers/Student.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StudentService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        //private readonly IStudentRepository _studentRepository;
+        //public StudentController(IStudentRepository studentRepository) => (_studentRepository) = (studentRepository);
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok("Hello from Student");
+        }
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            int result = id * 50;
+            return Ok(result);
+        }
+    }
+}
---
+++ WebAPIOcelot/TeacherService/Controllers/Teacher.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TeacherService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TeacherController : ControllerBase
+    {
+        //private readonly ITeacherRepository _repository;
+        //public TeacherController(ITeacherRepository repository) => _repository = repository;
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok("Hello from Teacher");
+        }
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            int result = id * 40;
+            return Ok(result);
+        }
+    }
+}
